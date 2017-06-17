6cf8facca480e10c4d8d1d48825eaf21ad225f8e
Sanjeev Premi <spremi@ymail.com>
2017-06-17 23:38:56 +0530

    Initial scaffolding
    Created via 'yo angular-fullstack:app BookVn'
    
    For reference:
      - NodeJS v6.9.1 LTS
      - generator-angular-fullstack v2.1.1
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('bookVnApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
