eafaa371b87cab2ed51e4223623029a904d95b4c
Sanjeev Premi <spremi@ymail.com>
2017-06-18 11:23:09 +0530

    server: Define invariants for data stored in files
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
-/**
- * Main application file
- */
+//
+// [BookVn] server/app.js
+//
+// Entry point to server application.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
 
 'use strict';
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//
+// Invariants for data in JSON files
+//
+global.SIGN_SUBJECTS  = 'BookVn.SUBJECTS';
+global.SIGN_BOOKS     = 'BookVn.BOOKS';
+global.SIGN_CHAPTERS  = 'BookVn.CHAPTERS';
 
 var express = require('express');
 var config = require('./config/environment');
