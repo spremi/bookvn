fdeba925e859adf52524deabd8b0df08273e56d6
Sanjeev Premi <spremi@ymail.com>
2017-06-18 23:09:04 +0530

    client: /image: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/image/image.controller.js
+++ client/app/image/image.controller.js
@@ -1,6 +1,41 @@
+//
+// [BookVn] client/app/image/image.controller.js
+//
+// Implements controller for state 'iamge'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
-  .controller('ImageCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ImageCtrl', function ($scope, $stateParams, PageSvc) {
+    $scope.self = $stateParams.pg;
+    $scope.argPrev = null;
+    $scope.argNext = null;
+
+    var prev = PageSvc.prev($stateParams.sub, $stateParams.bk, $stateParams.ch, $stateParams.pg);
+    var next = PageSvc.next($stateParams.sub, $stateParams.bk, $stateParams.ch, $stateParams.pg);
+
+    if (prev !== null) {
+      $scope.argPrev = {
+        sub: $stateParams.sub,
+        bk: $stateParams.bk,
+        ch: $stateParams.ch,
+        pg: prev
+      };
+    }
+
+    if (next !== null) {
+      $scope.argNext = {
+        sub: $stateParams.sub,
+        bk: $stateParams.bk,
+        ch: $stateParams.ch,
+        pg: next
+      };
+    }
   });
--- client/app/image/image.js
+++ client/app/image/image.js
@@ -1,11 +1,35 @@
+//
+// [BookVn] client/app/image/image.js
+//
+// Describes state 'image'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('image', {
-        url: '/image',
-        templateUrl: 'app/image/image.html',
-        controller: 'ImageCtrl'
+        url: '/image/:sub/:bk/:ch/:pg',
+        views: {
+          'header': {
+            templateUrl: 'templates/app-header.html',
+            controller: 'AppHeaderCtrl'
+          },
+          'footer': {
+            templateUrl: 'templates/app-footer.html',
+            controller: 'AppFooterCtrl'
+          },
+          '': {
+            templateUrl: 'app/image/image.html',
+            controller: 'ImageCtrl'
+          }
+        },
       });
   });
