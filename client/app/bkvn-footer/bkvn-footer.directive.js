dc1b787e01cea570a7c22738956faf0a236607a5
Sanjeev Premi <spremi@ymail.com>
2017-06-18 22:29:55 +0530

    client: bkvn-footer: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/bkvn-footer/bkvn-footer.directive.js
+++ client/app/bkvn-footer/bkvn-footer.directive.js
@@ -1,11 +1,26 @@
+//
+// [BookVn] client/app/bkvn-footer/bkvn-footer.directive.js
+//
+// Implements directive 'bkvn-footer'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .directive('bkvnFooter', function () {
     return {
+      scope: {},
       templateUrl: 'app/bkvn-footer/bkvn-footer.html',
       restrict: 'E',
-      link: function () {
+      controller: function ($scope) {
+        $scope.author = 'Sanjeev Premi';
+        $scope.donee  = 'Ananya';
       }
     };
   });
