8c9b1ca3b7f4eefc0b7b20a7f6ace1beabbaddec
Sanjeev Premi <spremi@ymail.com>
2017-06-18 22:51:42 +0530

    client: /page: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/page/page.controller.js
+++ client/app/page/page.controller.js
@@ -1,6 +1,32 @@
+//
+// [BookVn] client/app/page/page.controller.js
+//
+// Implements controller for state 'page'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
-  .controller('PageCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PageCtrl', function ($scope, $stateParams, PageSvc) {
+
+    $scope.NoPages = false;
+
+    PageSvc.init($stateParams.sub,
+                  $stateParams.bk,
+                  $stateParams.ch,
+                  parseInt($stateParams.f),
+                  parseInt($stateParams.l),
+                  parseInt($stateParams.s));
+
+    $scope.Pages = PageSvc.list($stateParams.sub, $stateParams.bk, $stateParams.ch);
+
+    if ($scope.Pages.length === 0) {
+      $scope.NoPages = true;
+    }
   });
--- client/app/page/page.js
+++ client/app/page/page.js
@@ -1,11 +1,35 @@
+//
+// [BookVn] client/app/page/page.js
+//
+// Describes state 'page'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('page', {
-        url: '/page',
-        templateUrl: 'app/page/page.html',
-        controller: 'PageCtrl'
+        url: '/page/:sub/:bk/:ch/:f/:l/:s',
+        views: {
+          'header': {
+            templateUrl: 'templates/app-header.html',
+            controller: 'AppHeaderCtrl'
+          },
+          'footer': {
+            templateUrl: 'templates/app-footer.html',
+            controller: 'AppFooterCtrl'
+          },
+          '': {
+            templateUrl: 'app/page/page.html',
+            controller: 'PageCtrl'
+          }
+        }
       });
   });
