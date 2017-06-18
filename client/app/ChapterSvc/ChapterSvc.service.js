ea4d4521f9cc81756ce05275d5029799c69e15f6
Sanjeev Premi <spremi@ymail.com>
2017-06-18 19:44:47 +0530

    client: chapter-svc: Implement list()
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/ChapterSvc/ChapterSvc.service.js
+++ client/app/ChapterSvc/ChapterSvc.service.js
@@ -1,6 +1,70 @@
+//
+// [BookVn] client/app/ChapterSvc/ChapterSvc.service.js
+//
+// Implements service 'ChapterSvc.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
-  .service('ChapterSvc', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('ChapterSvc', function ($q, $http) {
+
+    /**
+     * Encapsulates list of chapters configured for a book
+     * Flag 'resolved' is used to avoid repeated $http.get(),
+     * when no chapter was configured.
+     */
+    var Chapters = {
+      subjectId: '',
+      bookId: '',
+      list: [],
+      resolved: false
+    };
+
+
+    /**
+     * Get list of chapters in a book.
+     */
+    this.list = function (subjectId, bookId) {
+      var deferred = $q.defer();
+
+      if ((Chapters.resolved) &&
+          (Chapters.subjectId === subjectId) &&
+          (Chapters.bookId === bookId) &&
+          (Object.keys(Chapters.list).length !== 0)) {
+        return $q.when(Chapters.list);
+      }
+
+      Chapters.resolved = false;
+      Chapters.subjectId = subjectId;
+      Chapters.bookId = bookId;
+
+      var query = '/api/info/subject/' + subjectId + '/book/' + bookId + '/chapters';
+
+      $http.get(query).then(
+        function (result) {
+          Chapters.list = result.data.chapters;
+
+          //
+          // Inject 'step' into every chapter.
+          //
+          for (var i = 0; i < Chapters.list.length; i++) {
+            Chapters.list[i].step = result.data.step;
+          }
+
+          Chapters.resolved = true;
+
+          deferred.resolve(Chapters.list);
+        }, function () {
+          deferred.reject();
+        });
+
+      return deferred.promise;
+    };
   });
