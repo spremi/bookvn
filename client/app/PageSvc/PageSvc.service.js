6326157674699263ccbd8647c18fd1930f22d75d
Sanjeev Premi <spremi@ymail.com>
2017-06-18 22:48:14 +0530

    client: page-svc: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/PageSvc/PageSvc.service.js
+++ client/app/PageSvc/PageSvc.service.js
@@ -1,6 +1,122 @@
+//
+// [BookVn] client/app/PageSvc/PageSvc.service.js
+//
+// Implements service 'PageSvc.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .service('PageSvc', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+
+    /**
+     * Encapsulates list of pages in a chapter
+     */
+    var Pages = {
+      subjectId: '',
+      bookId: '',
+      chapterId: '',
+      list: []
+    };
+
+    /**
+     * Initialize the pages for specified chapter
+     */
+    this.init = function (subjectId, bookId, chapterId, first, last, step) {
+      Pages.subjectId = subjectId;
+      Pages.bookId    = bookId;
+      Pages.chapterId = chapterId;
+      Pages.list      = [];
+
+      var temp = [];
+
+      for (var pg = first; pg <= last; pg += step) {
+        temp.push(pg);
+      }
+
+      for (var i = 0; i < temp.length; i++) {
+        var page = {
+          self: temp[i],
+          prev: (i === 0 ? null : temp[i - 1]),
+          next: (i === (temp.length - 1) ? null : temp[i + 1]),
+        };
+
+        Pages.list.push(page);
+      }
+    };
+
+    /**
+     * Returns list of pages for specified chapter
+     */
+    this.list = function (subjectId, bookId, chapterId) {
+      if ((Pages.subjectId === subjectId) &&
+          (Pages.bookId === bookId) &&
+          (Pages.chapterId === chapterId)) {
+        return Pages.list;
+      } else {
+        return [];
+      }
+    };
+
+    /**
+     * Finds index of specified page in the list
+     */
+    var pgIndex = function (num) {
+      var found = false;
+      var index = -1;
+
+      num = parseInt(num);
+
+      for (var i = 0; (!found) && (i < Pages.list.length); i++) {
+        if (Pages.list[i].self === num) {
+          found = true;
+          index = i;
+        }
+      }
+
+      return index;
+    };
+
+
+    /**
+     * Returns 'previous' page, if it exists
+     */
+    this.prev = function (subjectId, bookId, chapterId, num) {
+      if ((Pages.subjectId === subjectId) &&
+          (Pages.bookId === bookId) &&
+          (Pages.chapterId === chapterId) &&
+          (Pages.list.length > 0)) {
+        var idx = pgIndex(num);
+
+        if ((idx !== -1) && (idx !== 0)) {
+          return Pages.list[idx - 1].self;
+        }
+      }
+
+      return null;
+    };
+
+    /**
+     * Returns 'next' page, if it exists
+     */
+    this.next = function (subjectId, bookId, chapterId, num) {
+      if ((Pages.subjectId === subjectId) &&
+          (Pages.bookId === bookId) &&
+          (Pages.chapterId === chapterId) &&
+          (Pages.list.length > 0)) {
+        var idx = pgIndex(num);
+
+        if ((idx !== -1) && (idx < (Pages.list.length - 1))) {
+          return Pages.list[idx + 1].self;
+        }
+      }
+
+      return null;
+    };
   });
