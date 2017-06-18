d072fcc76ca96ea744d2ff2c4121bcb8dcefdea8
Sanjeev Premi <spremi@ymail.com>
2017-06-18 11:21:24 +0530

    server: Add endpoint 'list'
    Via 'yo angular-fullstack:endpoint list'
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ server/api/list/list.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+
+// Get list of lists
+exports.index = function(req, res) {
+  res.json([]);
+};
--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/list', require('./api/list'));
   app.use('/api/info', require('./api/info'));
 
   // All undefined asset or api routes should return a 404
