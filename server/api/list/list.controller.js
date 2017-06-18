1039128322c21837221564b85f6b17751ec3c5f5
Sanjeev Premi <spremi@ymail.com>
2017-06-18 11:42:31 +0530

    server: api/list: Implement subjectList()
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- server/api/list/list.controller.js
+++ server/api/list/list.controller.js
@@ -1,8 +1,56 @@
+//
+// [BookVn] server/api/list/index.js
+//
+// Implements routes for endpoint 'list'
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
-var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
 
-// Get list of lists
+var PathCabinet = path.join(global.BASE_APP, 'server', 'assets', 'cabinet');
+
+
+/**
+ * Returns list of subjects.
+ *
+ * Each directory in 'PathCabinet' corresponds to a subject.
+ */
+exports.subjectList = function (req, res) {
+  fs.readdir(PathCabinet,
+    function (err, items) {
+      if (err) {
+        res.status(500)
+          .send('list: subjects: Error reading directory.');
+
+        return;
+      }
+
+      var subjects = [];
+
+      for (var i = 0; i < items.length; i++) {
+        var stat = fs.statSync(path.join(PathCabinet, items[i]));
+
+        if (stat.isDirectory()) {
+          subjects.push(items[i]);
+        }
+      }
+
+      res.status(200).json(subjects);
+    });
+};
+
+
+/**
+ * Default route - leave as-is
+ */
 exports.index = function(req, res) {
   res.json([]);
 };
