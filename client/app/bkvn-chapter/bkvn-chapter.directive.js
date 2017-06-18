3f2ad549b0d166d481f2537ed08762c99c04747a
Sanjeev Premi <spremi@ymail.com>
2017-06-18 21:09:44 +0530

    client: bkvn-chapter: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/bkvn-chapter/bkvn-chapter.directive.js
+++ client/app/bkvn-chapter/bkvn-chapter.directive.js
@@ -1,11 +1,34 @@
+//
+// [BookVn] client/app/bkvn-chapter/bkvn-chapter.directive.js
+//
+// Implements directive 'bkvn-chapter'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .directive('bkvnChapter', function () {
     return {
+      scope: {
+        chapter: '<'
+      },
       templateUrl: 'app/bkvn-chapter/bkvn-chapter.html',
       restrict: 'E',
-      link: function () {
+      controller: function ($scope, $stateParams) {
+        $scope.arg = {
+          sub: $stateParams.sub,
+          bk: $stateParams.bk,
+          ch: $scope.chapter.id,
+          f: $scope.chapter.first,
+          l: $scope.chapter.last,
+          s: $scope.chapter.step
+        };
       }
     };
   });
