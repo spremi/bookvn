70c2b71e6adb60b00f87e2558a46403c85be6cc1
Sanjeev Premi <spremi@ymail.com>
2017-06-18 22:35:15 +0530

    client: bkvn-header: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/bkvn-header/bkvn-header.directive.js
+++ client/app/bkvn-header/bkvn-header.directive.js
@@ -1,3 +1,15 @@
+//
+// [BookVn] client/app/bkvn-header/bkvn-header.directive.js
+//
+// Implements directive 'bkvn-header'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
