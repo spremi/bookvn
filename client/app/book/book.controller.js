90e8a981fe4536d568f9803b5be3edabd6b5cf69
Sanjeev Premi <spremi@ymail.com>
2017-06-18 21:02:19 +0530

    client: /book: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/book/book.controller.js
+++ client/app/book/book.controller.js
@@ -1,6 +1,18 @@
+//
+// [BookVn] client/app/book/book.controller.js
+//
+// Implements controller for state 'book'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
-  .controller('BookCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('BookCtrl', function ($scope, BookList) {
+    $scope.Books = BookList;
   });
--- client/app/book/book.js
+++ client/app/book/book.js
@@ -1,11 +1,40 @@
+//
+// [BookVn] client/app/book/book.js
+//
+// Describes state 'book'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('book', {
-        url: '/book',
-        templateUrl: 'app/book/book.html',
-        controller: 'BookCtrl'
+        url: '/book/:sub',
+        views: {
+          'header': {
+            templateUrl: 'templates/app-header.html',
+            controller: 'AppHeaderCtrl'
+          },
+          'footer': {
+            templateUrl: 'templates/app-footer.html',
+            controller: 'AppFooterCtrl'
+          },
+          '': {
+            templateUrl: 'app/book/book.html',
+            controller: 'BookCtrl'
+          }
+        },
+        resolve: {
+          BookList: function ($stateParams, BookSvc) {
+            return BookSvc.list($stateParams.sub);
+          }
+        }
       });
   });
