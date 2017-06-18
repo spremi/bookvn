bd9a3a9708031c73eed7eb62b67b43df1d6f11ca
Sanjeev Premi <spremi@ymail.com>
2017-06-18 21:07:46 +0530

    client: /chapter: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/chapter/chapter.controller.js
+++ client/app/chapter/chapter.controller.js
@@ -1,6 +1,18 @@
+//
+// [BookVn] client/app/chapter/chapter.controller.js
+//
+// Implements controller for state 'chapter'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
-  .controller('ChapterCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ChapterCtrl', function ($scope, ChapterList) {
+    $scope.Chapters = ChapterList;
   });
--- client/app/chapter/chapter.js
+++ client/app/chapter/chapter.js
@@ -1,11 +1,41 @@
+//
+// [BookVn] client/app/chapter/chapter.js
+//
+// Describes state 'chapter.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('chapter', {
-        url: '/chapter',
-        templateUrl: 'app/chapter/chapter.html',
-        controller: 'ChapterCtrl'
+        url: '/chapter/:sub/:bk',
+        views: {
+          'header': {
+            templateUrl: 'templates/app-header.html',
+            controller: 'AppHeaderCtrl'
+          },
+          'footer': {
+            templateUrl: 'templates/app-footer.html',
+            controller: 'AppFooterCtrl'
+          },
+          '': {
+            templateUrl: 'app/chapter/chapter.html',
+            controller: 'ChapterCtrl'
+          }
+        },
+        resolve: {
+          ChapterList: function ($stateParams, ChapterSvc) {
+            return ChapterSvc.list($stateParams.sub, $stateParams.bk);
+          }
+        }
+
       });
   });
