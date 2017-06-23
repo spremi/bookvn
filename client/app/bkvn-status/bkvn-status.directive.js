639bc504c61caeb04737796158649ba9b14d4426
Sanjeev Premi <spremi@ymail.com>
2017-06-23 20:49:06 +0530

    client: bkvn-status: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/bkvn-status/bkvn-status.directive.js
+++ client/app/bkvn-status/bkvn-status.directive.js
@@ -1,11 +1,61 @@
+//
+// [BookVn] client/app/bkvn-status/bkvn-status.directive.js
+//
+// Implements directive 'bkvn-status'.
+//
+// (c) 2017 Sanjeev Premi (spremi@ymail.com)
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//                          (http://spdx.org/licenses/BSD-3-Clause.html)
+//
+
+
 'use strict';
 
 angular.module('bookVnApp')
   .directive('bkvnStatus', function () {
     return {
+      scope: {},
       templateUrl: 'app/bkvn-status/bkvn-status.html',
       restrict: 'E',
-      link: function () {
+      controller: function ($scope, $state, $stateParams,
+                            SubjectSvc, BookSvc, ChapterSvc) {
+        var state  = $state.$current.name;
+
+        $scope.subject  = null;
+        $scope.book     = null;
+        $scope.chapter  = null;
+        $scope.page     = null;
+
+        //
+        // Set arguments for all previous states
+        //
+        switch(state) {
+          case 'book':
+            $scope.subject = SubjectSvc.name($stateParams.sub);
+            break;
+
+          case 'chapter':
+            $scope.subject = SubjectSvc.name($stateParams.sub);
+            $scope.book    = BookSvc.title($stateParams.sub, $stateParams.bk);
+            break;
+
+          case 'page':
+            $scope.subject = SubjectSvc.name($stateParams.sub);
+            $scope.book    = BookSvc.title($stateParams.sub, $stateParams.bk);
+            $scope.chapter = ChapterSvc.title($stateParams.sub, $stateParams.bk, $stateParams.ch);
+            break;
+
+          case 'image':
+            $scope.subject = SubjectSvc.name($stateParams.sub);
+            $scope.book    = BookSvc.title($stateParams.sub, $stateParams.bk);
+            $scope.chapter = ChapterSvc.title($stateParams.sub, $stateParams.bk, $stateParams.ch);
+            $scope.page    = $stateParams.pg;
+            break;
+
+          default:
+            break;
+        }
       }
     };
   });
