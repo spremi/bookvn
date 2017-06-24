e25c52fb906f629e9d8c7308a0d0ba178a566165
Sanjeev Premi <spremi@ymail.com>
2017-06-24 22:08:47 +0530

    build: Copy new templates to distribution
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -347,6 +347,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
+            'templates/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
             'index.html'
