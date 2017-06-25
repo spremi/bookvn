0b3f195d4add9258f4c9da57fd3874af5b15db94
Sanjeev Premi <spremi@ymail.com>
2017-06-25 17:16:26 +0530

    client: app-header: Add placeholder for package version
    This placeholder shall be replaced with actual
    version while building the distribution package.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/app/AppHeader/AppHeader.controller.js
+++ client/app/AppHeader/AppHeader.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('bookVnApp')
-  .controller('AppHeaderCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AppHeaderCtrl', function ($scope, $state) {
+    //
+    // Show version?
+    //
+    $scope.vflag = ($state.$current.name === 'main' ? true : false);
   });
