0c5bfdfdf777ecd5dd583a6f768b7c4c81f0f6bf
Sanjeev Premi <spremi@ymail.com>
2017-06-25 16:53:07 +0530

    Revert "build: Copy new templates to distribution"
    This reverts commit e25c52fb906f629e9d8c7308a0d0ba178a566165.
    
    Instead of copying template files as-is, include
    them in template cache.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -347,7 +347,6 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'templates/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
             'index.html'
